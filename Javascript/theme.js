e956f4e2671d1a7c5a0563e0bafb7c233b6551be
Yasamin Alizadeh <itsYasaminAlizadeh@gmail.com>
2022-03-27 20:41:52 +0430

    refactor(theme): removed button styles removed button styles from function

--- Javascript/theme.js
+++ Javascript/theme.js
@@ -13,9 +13,6 @@ const changeTheme = (theme) => {
       localStorage.setItem("theme", "light");
       document.body.classList.remove("theme--dark");
 
-      themeBtns.forEach((btn) => btn.classList.remove("theme__btn--active"));
-      lightThemeBtn.classList.add("theme__btn--active");
-
       document.body.classList.remove("theme--dark");
       document.getElementById("background").style.transform = "scaleY(1)";
       document.getElementById("background").style.transformOrigin = "top";
@@ -23,9 +20,6 @@ const changeTheme = (theme) => {
 
     case "dark":
       localStorage.setItem("theme", "dark");
-
-      themeBtns.forEach((btn) => btn.classList.remove("theme__btn--active"));
-      darkThemeBtn.classList.add("theme__btn--active");
 
       document.body.classList.add("theme--dark");
       document.getElementById("background").style.transform = "scaleY(0)";
@@ -45,10 +39,14 @@ const darkThemeBtn = document.getElementById("dark-theme-btn");
 
 lightThemeBtn &&
   lightThemeBtn.addEventListener("click", () => {
+    themeBtns.forEach((btn) => btn.classList.remove("theme__btn--active"));
+    lightThemeBtn.classList.add("theme__btn--active");
     changeTheme("light");
   });
 
 darkThemeBtn &&
   darkThemeBtn.addEventListener("click", () => {
+    themeBtns.forEach((btn) => btn.classList.remove("theme__btn--active"));
+    darkThemeBtn.classList.add("theme__btn--active");
     changeTheme("dark");
   });
